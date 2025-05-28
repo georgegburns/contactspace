60738282412b3f37e4e7a6a732e20bc970dc1bf1
georgegburns <88833145+georgegburns@users.noreply.github.com>
2025-05-28 19:15:50 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
    name="contact_space",
-   version="0.0.4",
+   version="0.1.01",
    description="SDK to interact with the ContactSpace REST API",
    author="George Burns",
    author_email="mrgeorgegburns@gmail.com",
