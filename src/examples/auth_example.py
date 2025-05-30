1e90bbd97e7a8828575f688e3a32aecffa317331
georgegburns <88833145+georgegburns@users.noreply.github.com>
2025-05-30 18:09:55 +0100

    Delete src directory

--- src/examples/auth_example.py
+++
@@ -1,6 +0,0 @@
-from contactspace import ContactSpace
-
-api_key = "EXAMPLE_API_KEY"
-url = "https://ukapithunder.makecontact.space/" # Update to region specific URL
-
-auth = ContactSpace(api_key, url)
